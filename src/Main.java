20c18637e24185db1ebf5efdd02a8f7bf06256c5
Robert Koenig <Robert Koenig>
2017-01-14 14:00:26 -0800

    Added change to search function

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,66 @@
+
+//imports
 import java.util.Scanner;
 
-/**
- * @author Rob
- *
- */
 public class Main {
-    protected String[] database;
+    protected String[][] database;
     private Scanner input;
 
     public Main() {
-        database = new String[0];
+        database = new String[0][2];
         input = new Scanner(System.in);
     }
 
-    /**
-     * @param name
-     */
-    public void add(final String entry) {
-        if (search(entry) != -1) {
-            String[] temp = new String[database.length + 1];
-            temp[database.length] = entry;
+    // add function, should check for duplicate entry, adding the new entry to
+    // the array if it is not a duplicate
+    public void add(final String name, final String number) {
+        System.out.println("starting add function");
+        if (search(name) == -1) {
+            System.out.println("creating new array");
+            String[][] temp = new String[database.length + 1][2];
             for (int i = 0; i < database.length; i++) {
+                System.out.println("copied entry at position " + i + " to new array");
                 temp[i] = database[i];
             }
+            temp[database.length][0] = name;
+            temp[database.length][1] = number;
             database = temp;
         }
     }
 
-    /**
-     * @param name
-     * @return returnType
-     */
+    // overload for add function, when it only calls a name
+    public void add(final String name) {
+        add(name, "");
+    }
+
+    // search function, should search for the entered string as a name
     public int search(final String name) {
+        System.out.println("starting search function");
         if (name != null) {
             for (int i = 0; i < database.length; i++) {
-                if (database[i].toLowerCase().contains(name.toLowerCase())) {
+                if (database[i][0].toLowerCase() == name.toLowerCase()) {
+                    System.out.println("search returned true at position " + i);
                     return i;
                 }
             }
         }
+        System.out.println("search returned null!");
         return -1;
     }
 
-    /**
-     * 
-     */
+    // displayall, should display all entries
     public void displayAll() {
-
+        System.out.println("#\tName\tPhone Number");
         for (int i = 0; i < database.length; i++) {
-            System.out.printf("%40s\n", database[i]);
-            // System.out.println(String.format("%s", database[i]));
+            System.out.println(i + "\t" + database[i][0] + "\t" + database[i][1]);
         }
     }
 
-    /**
-     * @param name
-     * @return
-     */
+    // remove, should function without any editing
     public boolean remove(final String name) {
         int pos = search(name);
         if (pos >= 0) {
-            String[] temp = new String[database.length - 1];
+            String[][] temp = new String[database.length - 1][2];
             System.arraycopy(database, 0, temp, 0, pos);
             System.arraycopy(database, pos + 1, temp, pos, database.length - pos - 1);
             database = temp;
@@ -69,17 +69,16 @@ public class Main {
         return false;
     }
 
-    /**
-     * 
-     */
+    // UI function, should not need editing
     public void displayMenu() {
-        System.out.println("\n\n\n1) Add");
+        System.out.println("1) Add");
         System.out.println("2) Delete");
         System.out.println("3) Search");
         System.out.println("4) Display All");
-        System.out.println("5) Exit\n");
+        System.out.println("5) Exit");
     }
 
+    // get user choice and return it
     public int getChoice() {
         int choice = 4;// default
         boolean done = false;
@@ -92,28 +91,23 @@ public class Main {
             if (choice > 0 && choice <= 5) {
                 done = true;
             } else {
-                System.out.println("\nYour choice is incorrect, please try again");
+                System.out.println("Invalid choice! Please try again.");
             }
         }
         return choice;
     }
 
+    // used in addperson to check if the entry already exists
     public boolean checkPersonAdded(String entryName, String entryNumber) {
-        boolean trueOrFalse = false;
         for (int i = 0; i < database.length; i++) {
-            for (int j = 0; j < database.length; j++) {
-                if (database[i].toLowerCase().contains(entryName.toLowerCase())
-                        || database[j].toLowerCase().contains(entryNumber.toLowerCase())) {
-                    trueOrFalse = true;
-                }
+            if (database[i][0].toLowerCase().contains(entryName.toLowerCase()) || database[i][1] == entryNumber) {
+                return true;
             }
         }
-        return trueOrFalse;
+        return false;
     }
 
-    /**
-     * 
-     */
+    // UI for add() function
     public void addPerson() {
         String name = "";
         String phone = "";
@@ -127,15 +121,13 @@ public class Main {
         } catch (Exception e) {
         }
         if (checkPersonAdded(name, phone) == false) {
-            add(name + " " + phone);
+            add(name, phone);
         } else {
             System.out.println("This entry is already added.");
         }
     }
 
-    /**
-     * 
-     */
+    // ui for remove() function
     public void deletePerson() {
         String name = "";
         try {
@@ -151,9 +143,7 @@ public class Main {
         }
     }
 
-    /**
-     * 
-     */
+    // ui for search() function
     public void findPerson() {
         String name = "";
         try {
@@ -164,20 +154,13 @@ public class Main {
         }
         int pos = search(name);
         if (pos >= 0) {
-            // ADD YOUR DISPLAY CODE HERE TO DISPLAY A SINGLE PERSON'S INFO
-            for (String look : database) {
-                if (look.contains(name)) {
-                    System.out.println(database[pos]);
-                }
-            }
+            System.out.println(database[pos][0] + "'s Phone Number: " + database[pos][1]);
         } else {
             System.out.println("No such person");
         }
     }
 
-    /**
-     * 
-     */
+    // main execution of ui
     public void run() {
         int choice = 0;
         do {
@@ -205,7 +188,3 @@ public class Main {
         new Main().run();
     }
 }
-
-// 8:44 PM - hush: entry i of database array is a 2 entry array of strings,
-// database[i][0] being the name, database[i][1] being the phone number
-// 8:47 PM - hush: database[i]=name+"\t"+number
